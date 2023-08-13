297a375b7aff91b298ce6df5d0ae9b52d8292693
Dante <mbnjoomla@gmail.com>
2023-08-13 10:25:19 +0200

    Add files via upload

---
+++ Paraph.py
@@ -0,0 +1,17 @@
+print( "aqui","dentro","separado ""\n" "por   ", sep="**" , end="")
+print()
+print("ahora viene seguido  en otra linea (Dante)")
+print(1e-22*2)
+
+
+
+
+
+
+
+
+
+
+print("Dime algo...")
+anything = input("RORORORORORSJDJ")
+print("Mmm...", anything, "...¿en serio?")
